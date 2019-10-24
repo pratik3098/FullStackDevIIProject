eeda39e09894ae566b6e3dcf423fd50d999a6135
Pratik Patil <pppatil@yorku.ca>
2019-10-24 19:05:52 -0400

    modules_added

--- server/functions/index.js
+++ server/functions/index.js
@@ -1,10 +1,14 @@
 const functions = require('firebase-functions');
 const express=require('express')
+
 const app = express()
 
 
 app.get('/timestamp',(request,response)=>{
     response.send(`${Date.now()}`);
 })
-
+app.get('/timestamp-cached',(request,response)=>{
+    response.set('Cache-Control','public, max-age=300, s-maxage=600')
+    response.send(`${Date.now()}`);
+})
 exports.app = functions.https.onRequest(app)
