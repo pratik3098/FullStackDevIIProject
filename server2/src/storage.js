e68a00beccd1c209ae7a76f896dcc17fd1c51fcb
pratik3098 <pratik3098@gmail.com>
2019-10-25 00:58:07 -0400

    Bucket upload/deletion functions added

--- server2/src/storage.js
+++ server2/src/storage.js
@@ -1,41 +1,83 @@
+'use strict'
 const {Storage}=require('@google-cloud/storage')
 const gcs= new Storage({
     projectId: 'fullstackdeviiproject-f8413',
 })
 const default_imageCloud= 'gs://fullstackdeviiproject-f8413.appspot.com/imageCloud/'
 const default_userImg='gs://fullstackdeviiproject-f8413.appspot.com/userImages/'
-const imgCloud = gcs.bucket(default_imageCloud)
-const userImg  =gcs.bucket(default_userImg)
 
 exports.adduserBucket= function adduserBucket(userName){
    let bucketName= default_imageCloud+ userName
    gcs.createBucket(bucketName)
   .then(() => {
-    console.log("Bucket successfuly created.");
+    console.log("Log: Bucket creation success");
   })
   .catch(err => {
     console.error('Error:', err);
   });
 }
 
-exports.adduserProfilePic=function adduserProfilePic(){
-     
-}
+exports.adduserProfilePic=async function adduserProfilePic(userName, pic){
+  if(pic.charAt(pic.length-1)=='/')
+     pic[pic.length-1]=' '
+  await gcs.bucket(default_userImg).upload(pic,{
+    gzip: true,
+    metadata:{
+       cacheControl: 'public, max-age=31536000'
+    },
+  }
+  ).then(()=>{
+    console.log("Log: File upload success")
+  }).catch(err=>{
+    console.error('Error: Cannot upload file', err)
+  } 
+  )
 
-async deleteUserStorage(userName){
+  let names=pic.split('/')
+  let name=names[names.length-1]
+   
+  await gcs.bucket(default_userImg).file(name).move(userName).then(()=>{
+    console.log("Log: File rename success")
+  }).catch(err=>{
+    console.error('Error: Cannot rename file', err)
+  })
+  
+}
+exports.addUserImages=async function addUserImage(username,filename){
+  if(pic.charAt(pic.length-1)=='/')
+  pic[pic.length-1]=' '
+  let bucketName= default_imageCloud+ userName
+  await gcs.bucket(bucketName).upload(filename,{
+    gzip: true,
+    metadata:{
+       cacheControl: 'public, max-age=31536000'
+    },
+  }).then(()=>{
+    console.log("Log: File upload success")
+  }).catch(err=>{
+    console.error('Error: Cannot upload file',err)
+  })
+}
+exports.deleteUserStorage=async function deleteUserStorage(userName){
     let bucketName= default_imageCloud+ userName
-    await gcs.bucket(bucketName).delete().catch(err => {
-        console.error('Error:',err)
+    await gcs.bucket(bucketName).delete().then(()=>{
+      console.log("Log: File deletion sucess")
+    }).catch(err => {
+        console.error('Error: Cannot delete file',err)
     })
-    await gcs.bucket(default_userImg).file(userName).delete().catch(err=>{
+    await gcs.bucket(default_userImg).file(userName).then(()=>{
+      console.log("Log: File deletion sucess")
+    }).delete().catch(err=>{
         console.error(`Error: Cannot remove file ${userName}`,err)
     })   
 }
-async deleteImageFile(userName,fileName){
+exports.deleteImageFile=async function deleteImageFile(userName,fileName){
     let bucketName= default_imageCloud+ userName
-    await gcs.bucket(bucketName).file(fileName).delete().catch(err=>{
+    await gcs.bucket(bucketName).file(fileName).delete().then(()=>{
+       console.log("Log: File deletion sucess")
+    }).catch(err=>{
          console.error(`Error: Cannot remove file ${fileName}`,err)
     })
 }
-this.adduserBucket('Pratik')
-this.deleteUserStorage('Pratik')
+// this.adduserBucket('Pratik')
+// this.deleteUserStorage('Pratik')
