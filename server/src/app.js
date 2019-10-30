62a9baec8a817cb2eb358debd53383252736c378
Pratik Patil <101284380@georgebrown.ca>
2019-10-30 13:43:45 -0400

    .

--- server/src/app.js
+++ server/src/app.js
@@ -1,42 +1,47 @@
+const express=require('express')
 const path=require('path')
-const fs = require('fs')
 const firebase=require('firebase')
-const express= require('express')
-const bodyParser= require('body-parser');
+const bodyParser= require('body-parser')
+
 const app = express()
+const firebaseapp = firebase.initializeApp({ 
+apiKey: 'AIzaSyDzZ3EvtGsdX8yGpv7ySn0w2OS2Ov5-JuU',
+//authDomain: '<your-auth-domain>',
+//databaseURL: '<your-database-url>',
+projectId: 'fullstackdeviiproject-f8413',
+//storageBucket: '<your-storage-bucket>',
+//messagingSenderId: '<your-sender-id>'
+})
+
+app.set('title','My Gallery')
 app.set('view engine','hbs')
 app.set('views',path.join(__dirname,"../views"))
 app.use(express.static("../views"))
 app.use(bodyParser.urlencoded({extended: false}))
 
 app.get('',(req,res)=>{
+    
     res.render('index')    
 })
-
-
-app.post('/submit',(req, res)=>{
-     try{
-        if(isValidKey(req.body.privateKey))
-          mywallet= new Wallet(req.body.privateKey,req.body.provider)
-     else if(req.body.seed.length>0)
-          mywallet =new Wallet(req.body.privateKey,req.body.provider,req.body.seed)
-     else
-          throw "Invalid key"
-     }
-     catch(err){ 
-         res.redirect('/')
-     }
-    res.redirect('/mywallet')
-
+app.post('/signup',(req,res)=>{
+    
+    res.render('index')    
 })
-app.get('/mywallet',(req,res)=>{
-    if(typeof mywallet == "undefined")
-      res.redirect('/')
-    else
-    renderWalletData(res)
+app.get('/about',(req,res)=>{
+    res.render('about')
+})
+app.get('/profile',(req,res)=>{
+    res.render('profile',
+    { firstname: 'Mike',
+      lastname: 'Snow',
+      username: 'mikey455',
+      //userimg: '',
+      email: 'abcd@gmail.com', 
+      imgcount: '10',
+      userSince: '01/13/2019'
+     })
 })
 
 app.listen(8080,()=>{
     console.log("Server is running on port: 8080")
-}) 
-
+})
