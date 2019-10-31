81976cd3cdc3c89417d378a0777409e0d5db0353
Pratik Patil <101284380@georgebrown.ca>
2019-10-31 19:22:52 -0400

    databse added

--- server/src/database.js
+++ server/src/database.js
@@ -1,10 +1,17 @@
-const firebase=require['firebase']
-const database=firebase.database()
-database.enableLogging(true, true)
+const firebase=require('firebase')
+const admin = require('firebase-admin')
+const path=require('path')
+admin.initializeApp({
+    credential: admin.credential.cert(path.resolve(__dirname,'../../serviceKey.json')),
+    databaseURL: 'https://fullstackdeviiproject-f8413.firebaseio.com'
+})
 
+const db = admin.database()
 
+let ref = db.ref("restricted_access/secret_document")
 
-
-exports.refreshDatabase=function refreshDatabase(){
-    database.goOnline()
-}
+ref.on("Pratik", function(snapshot) {
+    console.log(snapshot.val());
+  }, function (errorObject) {
+    console.log("The read failed: " + errorObject.code);
+  })
