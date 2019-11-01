4faf39271848910b82a33fa82178870ff08edca6
Pratik Patil <pppatil@yorku.ca>
2019-10-31 22:20:05 -0400

    firestore renamed

--- server/src/database.js
+++ server/src/database.js
@@ -1,24 +1,30 @@
-//const firebase=require('firebase')
 const admin = require('firebase-admin')
 const path=require('path')
 admin.initializeApp({
     credential: admin.credential.cert(path.resolve(__dirname,'../../../serviceKey.json')),
     databaseURL: 'https://fullstackdeviiproject-f8413.firebaseio.com'
 })
+const firestore=admin.firestore()
 
-let database = admin.database()
-
-//let ref = database.ref("restricted_access/secret_document")
-
-/*ref.on("Pratik", function(snapshot) {
-    console.log(snapshot.val());
-  }, function (errorObject) {
-    console.log("The read failed: " + errorObject.code);
-  }) */
-  let postData = {
-    firstName: 'John',
-    lastName: 'Snow',
-    uid: 2
-  }
-  let newPostKey= database.ref('users/').push().key
-  database.ref('users/3').set(postData)
+exports.setUserData=function setUserData(userName,userData)
+{  
+  userName=userName.toLowerCase()
+  firestore.collection('users').doc(userName).set(userData).catch(err=>{
+      console.error(err)
+  })
+}
+exports.getUserData=async function getUserData(userName)
+{  
+    userName=userName.toLowerCase()
+    let res= await firestore.collection('users').doc(userName).get().catch(err=>{
+      console.error(err)
+  })
+    if(typeof res.data() === "undefined")
+      return null
+    else 
+    return res.data()
+}
+/*
+this.setUserData('john200',{ firstName: 'John', lastName: 'Snow'})  // -- Working fine
+Promise.resolve(this.getUserData('1')).then(value=>{console.log(value)}).catch(err=>{console.log(err)})  // --Working fine
+//*/
